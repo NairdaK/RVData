ffb03d5b39a60b2371e337d9af9085cbbb93e00f
akaminsk <akaminsk@lx25.lsw.uni-heidelberg.de>
2026-07-08 16:15:25 +0200

    Add CARMENES instrument files

---
+++ rvdata/instruments/carmenes/level2.py
@@ -0,0 +1,13 @@
+from astropy.io import fits
+from astropy.table import Table
+from astropy import constants
+import numpy as np
+import pandas as pd
+import os
+import warnings
+from collections import OrderedDict
+
+# import base class
+from rvdata.core.models.level2 import RV2
+
+
